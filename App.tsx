cfcf32fd784a0466286b0cf92e14c41929ef201b
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2023-10-20 23:49:38 +0500

    fixed file structure and added an image!
    fixed file structure and added an image!

--- App.tsx
+++ App.tsx
@@ -30,7 +30,9 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import { startMapper } from 'react-native-reanimated';
 import Svg, { Path } from 'react-native-svg'
-import SvgComponent from './component/SvgComonent';
+import SvgComponent from './src/components/SvgComponent';
+
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
@@ -50,14 +52,19 @@ function App(): JSX.Element {
 
     <View style={{ flex: 1, backgroundColor: 'white' }}>
       <View style={{ marginTop: 90 }}></View>
-      <View style={{ flex: 0.5, }}></View>
-      <View style={{ flex: 0.9, }}></View>
+      <View style={{ flex:1 , justifyContent:'center',alignItems:'center'}}>
+
+        <Image source={require('./src/assets/Logo.png')}   style={{width: 250, height: 250 , resizeMode:'contain'}}  ></Image>
+
+
+
+      </View>
 
 
 
 
       <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-        <SvgComponent />
+    <SvgComponent/>
         <View style={{
           position: 'absolute',
           top: 0,
