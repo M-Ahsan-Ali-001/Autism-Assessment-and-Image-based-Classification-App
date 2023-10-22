b6a1ec86b71c6a9d81840d061d2b94187d214694
momintlh <77355191+momintlh@users.noreply.github.com>
2023-10-22 19:51:28 +0500

    SHIFT ALT F

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
-import Svg,{Circle} from 'react-native-svg'
+import Svg, {Circle} from 'react-native-svg';
 import {
   SafeAreaView,
   ScrollView,
@@ -28,44 +28,44 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import IntroScreen from './src/screens/IntroScrn';
 import IntroScreen2 from './src/screens/introScrn2';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import IntroScreen3 from './src/screens/introScrn3';
 
-
 function App(): JSX.Element {
-
-
- 
   const Stack = createNativeStackNavigator();
-
 
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home"
-    screenOptions={{
-      headerShown: false, // Hide the header for all screens in this stack
-    }}
-  
-
-    >
-        <Stack.Screen name="Home" component={IntroScreen} options={{ animation:"slide_from_right" }} // Disable animation for this screen
- />
-        <Stack.Screen name="Details" component={IntroScreen2} options={{ animation:"slide_from_right" }} />
-        <Stack.Screen name="Screen3" component={IntroScreen3} options={{ animation:"slide_from_right" }} />
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false, // Hide the header for all screens in this stack
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={IntroScreen}
+          options={{animation: 'slide_from_right'}} // Disable animation for this screen
+        />
+        <Stack.Screen
+          name="Details"
+          component={IntroScreen2}
+          options={{animation: 'slide_from_right'}}
+        />
+        <Stack.Screen
+          name="Screen3"
+          component={IntroScreen3}
+          options={{animation: 'slide_from_right'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
- 
   );
 }
-
 
 const styles = StyleSheet.create({
   page: {
     width: '100%', // Adjust the width or height based on your layout
     height: '100%', // to fit the viewport
-
-   
   },
 });
 export default App;
--- src/screens/introScrn2.jsx
+++ src/screens/introScrn2.jsx
@@ -1,4 +1,4 @@
-import React, { State } from "react";
+import React, {State} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -11,8 +11,6 @@ import {
   Alert,
 } from 'react-native';
 
-
-
 import {
   Colors,
   DebugInstructions,
@@ -21,86 +19,83 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler';
-import { RadioButton } from 'react-native-paper';
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from 'react-native-gesture-handler';
+import {RadioButton} from 'react-native-paper';
 
-
-
-function IntroScreen2({ navigation }) {
-
-  const [textFooter, changeTextFooter] = React.useState('AQ 10 question')
+function IntroScreen2({navigation}) {
+  const [textFooter, changeTextFooter] = React.useState('AQ 10 question');
   const [shapeCond, setShapeCond] = React.useState(false);
 
-
-
-  const handleGesture = (evt) => {
-    const { nativeEvent } = evt;
+  const handleGesture = evt => {
+    const {nativeEvent} = evt;
 
     if (nativeEvent.velocityX > 0) {
       console.log('Swipe right');
 
-      navigation.navigate('Home')
-
+      navigation.navigate('Home');
     }
-
 
     if (nativeEvent.velocityX < 0) {
       console.log('Swipe rleft');
 
-      changeTextFooter("changed text")
-      navigation.navigate("Screen3")
-      setShapeCond(false)
+      changeTextFooter('changed text');
+      navigation.navigate('Screen3');
+      setShapeCond(false);
     }
   };
 
   return (
-    <GestureHandlerRootView style={{ backgroundColor: 'white', flex: 1 }}>
+    <GestureHandlerRootView style={{backgroundColor: 'white', flex: 1}}>
       <PanGestureHandler onGestureEvent={handleGesture}>
-        <View style={{ backgroundColor: 'white', flex: 1 }}>
-          <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 90, height: 250, width: 250 }}>
+        <View style={{backgroundColor: 'white', flex: 1}}>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 90,
+              height: 250,
+              width: 250,
+            }}>
             {/* <Image source={require('../assets/Logo.png')} style={{ height: 250, width: 250, resizeMode: 'contain' }}></Image> */}
           </View>
 
-
-          <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-            <View style={{ justifyContent: 'flex-end' }}>
+          <View style={{flex: 1, justifyContent: 'flex-end'}}>
+            <View style={{justifyContent: 'flex-end'}}>
               {/* {shapeCond ?(<SvgComponent1/>): (<SvgComponent/>)} */}
               {/* <SvgComponent1/> */}
-              <View style={{ justifyContent: 'flex-end' }}>
-
-                <Image source={require('../assets/wave2.png')}
+              <View style={{justifyContent: 'flex-end'}}>
+                <Image
+                  source={require('../assets/wave2.png')}
                   style={{
-                    width: '100%'
-                    , height: '90%',
+                    width: '100%',
+                    height: '90%',
                     margin: 0,
                     padding: 0,
                     top: 15,
                     justifyContent: 'flex-end',
-                    resizeMode: 'contain'
-
-
-
-
-                  }
-
-
-                  }></Image>
-              </View>
-
-              <View style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 30,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-                <Text style={{ fontWeight: '900', fontSize: 18, color: 'black' }}>{textFooter}</Text>
+                    resizeMode: 'contain',
+                  }}></Image>
               </View>
 
               <View
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 30,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Text style={{fontWeight: '900', fontSize: 18, color: 'black'}}>
+                  {textFooter}
+                </Text>
+              </View>
 
+              <View
                 style={{
                   position: 'absolute',
                   top: 150,
@@ -109,31 +104,18 @@ function IntroScreen2({ navigation }) {
                   bottom: 0,
                   justifyContent: 'center',
                   alignItems: 'center',
-                  flexDirection: 'row'
-                }}
-              >
-
-                <RadioButton ></RadioButton>
+                  flexDirection: 'row',
+                }}>
+                <RadioButton></RadioButton>
                 <RadioButton status="checked"></RadioButton>
-                <RadioButton  ></RadioButton>
+                <RadioButton></RadioButton>
               </View>
-
-
-
             </View>
           </View>
-
-
         </View>
-
       </PanGestureHandler>
-
     </GestureHandlerRootView>
-
-
-
-
-  )
+  );
 }
 
 export default IntroScreen2;
